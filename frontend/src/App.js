badd29720211959e207292967ae4d2f2f70ed71c
Vakantanka <laszlo.fenyvesi@gmail.com>
2022-05-10 20:54:47 +0200

    color palette: purple, deepOrange

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,11 +18,30 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import red from '@mui/material/colors/red';
+import pink from '@mui/material/colors/pink';
 import purple from '@mui/material/colors/purple';
+import deepPurple from '@mui/material/colors/deepPurple';
+import indigo from '@mui/material/colors/indigo';
+import blue from '@mui/material/colors/blue';
+import lightBlue from '@mui/material/colors/lightBlue';
+import cyan from '@mui/material/colors/cyan';
+import teal from '@mui/material/colors/teal';
+import green from '@mui/material/colors/green';
+import lightGreen from '@mui/material/colors/lightGreen';
+import lime from '@mui/material/colors/lime';
+import yellow from '@mui/material/colors/yellow';
+import amber from '@mui/material/colors/amber';
+import orange from '@mui/material/colors/orange';
+import deepOrange from '@mui/material/colors/deepOrange';
+import brown from '@mui/material/colors/brown';
+import grey from '@mui/material/colors/grey';
+import blueGrey from '@mui/material/colors/blueGrey';
 
 const theme = createTheme({
   palette: {
     primary: purple,
+    secondary: deepOrange
   },
 });
 
